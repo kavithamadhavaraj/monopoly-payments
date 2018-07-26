6882ebfd5d7aa7a1fea8b89c406184482b67453d
kavitha <kavitha@myquadrisk.in>
2018-07-26 12:41:45 +0530

    Heroku changees

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const forceSSL = function() {
 // Instruct the app
 // to use the forceSSL
 // middleware
-//app.use(forceSSL());
+app.use(forceSSL());
 
 
 
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as oauth from 'oauth';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  private  Oauth2 = oauth.OAuth2;
   constructor() { }
 
   ngOnInit() {
@@ -14,6 +14,20 @@ export class LoginComponent implements OnInit {
 
   googleLogin() {
     console.log('googlelogin');
+    const googleConsumerKey = '462871257136-hedggfdor0mchtgschjj2fuv4dfphamk.apps.googleusercontent.com';
+     const googleConsumerSecret = 'ixQKfkok1VXb0hYKSt0HacDa';
+     const oauth2 = new oauth.OAuth2(googleConsumerKey,
+      googleConsumerSecret,
+       'https://accounts.google.com/o/oauth2/',
+        'auth',
+        'token',
+       null);
+     oauth2.getOAuthAccessToken(
+       '',
+       {'grant_type': 'client_credentials'},
+       function (e, access_token, refresh_token, results){
+          console.log('bearer: ', access_token);
+       });
   }
 
   fbLogin() {
